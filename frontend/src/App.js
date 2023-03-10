b1f8aead4605f1e43c70a4f8aa27490a88d85157
ezmaa <ezinmark@gmail.com>
2023-03-10 14:31:53 -0500

    config styles with layout and global styles, add background file to components and config file to be animated using keyframes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
-
+import styled from "styled-components";
+import Background from "./components/Background/Background";
+import { MainLayout } from "./styles/Layouts";
+// import image 
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled className="App">
+      <Background></Background>
+      <MainLayout>
+        
+      </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+height: 100vh;
+background-image: url({});
+position: relative;
+main{
+  flex: 1;
+  background: rgba(252, 246, 249, 0.78);
+  border: 3px solid #FFFFFF;
+  backdrop-filter: blur(4.5px);
+  border-radius: 32px;
+  overflow-x: hidden;
+  &::-webkit-scrollbar{
+    width: 0;
+  }
+}
+`
 
 export default App;
